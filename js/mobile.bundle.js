eda7331aed93b2d426dd3a59ef427e6a351f746b
CJ Ting <fatelovely1128@gmail.com>
2017-06-10 12:31:00 +0800

    Rebuilding site Sat Jun 10 12:31:00 CST 2017

--- js/mobile.bundle.js
+++ js/mobile.bundle.js
@@ -1,1 +1,65 @@
-!function(e){function r(o){if(t[o])return t[o].exports;var a=t[o]={exports:{},id:o,loaded:!1};return e[o].call(a.exports,a,a.exports,r),a.loaded=!0,a.exports}var t={};return r.m=e,r.c=t,r.p="",r(0)}([function(e,r){var t=document.querySelector(".post__header");if(t){var o=t.getAttribute("data-cover");o=o.replace("large","bmiddle"),t.style.backgroundImage="url("+o+")"}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	/*
+	* @Author: CJ Ting
+	* @Date: 2016-11-04 13:49:49
+	* @Email: cj.ting@fugetech.com
+	*/
+
+	var postHeader = document.querySelector(".post__header")
+	if(postHeader) {
+	  var url = postHeader.getAttribute("data-cover")
+	  url = url.replace("large", "bmiddle")
+	  postHeader.style.backgroundImage = "url(" + url + ")"
+	}
+
+	// change disqus info
+	// document.querySelector(".dsq-brlink").innerText = "disable disqus for mobile devices"
+
+
+/***/ })
+/******/ ]);
