86f13c10d71e3e0479d95bc5334b85c58c66713d
CJ <cjting1128@gmail.com>
2020-07-03 01:16:15 +0800

    Rebuilding site: Fri Jul  3 01:16:15 CST 2020

--- asset/desktop.js
+++ asset/desktop.js
@@ -1,6 +1,60 @@
-// init gittalk
-function initGitalk() {
-  if(document.querySelector("#post__comments")) {
+;(function() {
+  function initGitalk() {
+  }
+
+  var notyf = new Notyf()
+
+  // replace post header image
+  var $postHeader = $(".post__header")
+  if($postHeader) {
+    $postHeader.style.backgroundImage = `url(${$postHeader.dataset.cover})`
+  }
+
+  $$("pre > code").forEach(function(ele) {
+    var div = document.createElement("div")
+    div.setAttribute("class", "post__content__code")
+  })
+
+  // add copy code buttn
+  $$(".highlight pre").forEach(function(ele, index) {
+    var id = "code-block-" + index
+    ele.setAttribute("id", id)
+
+    var icon = document.createElement("i")
+    icon.classList.add("iconfont", "icon-copy", "copy-code-btn")
+    icon.setAttribute("data-clipboard-target", "#" + id)
+    ele.parentElement.appendChild(icon)
+  })
+
+  var clipboard = new ClipboardJS(".copy-code-btn")
+
+  clipboard.on("success", function(e) {
+    notyf.success("Copied")
+    e.clearSelection()
+  })
+
+  // make all links opened at new tab
+  $$(".post__content a").forEach(function(ele){
+    ele.setAttribute("target", "_blank")
+  })
+
+  // make images zoomable
+  $$(".post__content img").forEach(function(ele) {
+    var f = function() {
+      if(ele.naturalWidth > ele.width) {
+        ele.dataset.action = "zoom"
+      }
+    }
+
+    if(ele.naturalWidth == 0) {
+      ele.onload = f
+    } else {
+      f()
+    }
+  })
+
+  // init gitalk
+  if(document.querySelector("#post__comments") && location.hostname !== "localhost") {
     const gitalk = new Gitalk({
       clientID: "f26cc90ab221487c7c8f",
       clientSecret: "fed1fa4d13c28f37a3a9087c6bb5585e14a87439",
@@ -13,65 +67,17 @@ function initGitalk() {
     })
     gitalk.render("post__comments")
   }
-}
 
-var notyf = new Notyf()
-
-;(function() {
-  // replace post header image
-  var $postHeader = $(".post__header")
-  if($postHeader) {
-    $postHeader.style.backgroundImage = `url(${$postHeader.dataset.cover})`
+  // init tocbot
+  if(document.querySelector(".post__toc")) {
+    tocbot.init({
+      tocSelector: ".post__toc",
+      contentSelector: ".post__content",
+      headingSelector: "h1, h2, h3, h4, h5, h6",
+      collapseDepth: 6,
+      positionFixedSelector: ".post__toc",
+      positionFixedClass: "post__toc--fixed",
+    })
   }
-
-  $$("pre > code").forEach(function(ele) {
-    var div = document.createElement("div")
-    div.setAttribute("class", "post__content__code")
-    // wrapElement(ele.parentNode, div)
-  })
-
-  // add copy code buttn
-  $$(".highlight pre").forEach(function(ele, index) {
-    var id = "code-block-" + index
-    ele.setAttribute("id", id)
-
-    var btn = document.createElement("span")
-    btn.innerText = "copy"
-    btn.classList.add("copy-code-btn")
-    btn.setAttribute("data-clipboard-target", "#" + id)
-    ele.parentElement.appendChild(btn)
-  })
-
-  var clipboard = new ClipboardJS(".copy-code-btn")
-
-  clipboard.on("success", function(e) {
-    notyf.success("Copied")
-    e.clearSelection()
-  })
-
-  // make all links opened at new tab
-  $$(".post__body a").forEach(function(ele){
-    ele.setAttribute("target", "_blank")
-  })
-
-  // make images zoomable
-  function magnificPopup(ele) {
-    if(ele.naturalWidth > ele.width) {
-      ele.classList.add("u-cursor-zoom-in")
-      $(ele).magnificPopup({
-        type: "image",
-        items: {
-          src: ele.getAttribute("src"),
-        },
-      })
-    }
-  }
-
-  $$(".post__body img").forEach(function(ele) {
-    if(ele.naturalWidth > ele.width) {
-      ele.dataset.action = "zoom"
-    }
-  })
-
-  initGitalk()
 })()
+
